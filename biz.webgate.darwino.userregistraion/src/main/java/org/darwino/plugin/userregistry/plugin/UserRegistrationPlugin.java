df3d505035b92b3f0c840a7d662de8d660a0d593
Christian Guedemann <christian.guedemann@webgate.biz>
2016-10-02 21:24:33 +0200

    REST API is loaded

--- biz.webgate.darwino.userregistraion/src/main/java/org/darwino/plugin/userregistry/plugin/UserRegistrationPlugin.java
+++ biz.webgate.darwino.userregistraion/src/main/java/org/darwino/plugin/userregistry/plugin/UserRegistrationPlugin.java
@@ -3,8 +3,10 @@ package org.darwino.plugin.userregistry.plugin;
 import java.util.List;
 
 import org.darwino.plugin.userregistry.ProfileServiceFactory;
+import org.darwino.plugin.userregistry.bean.UserRegistrationBean;
 import org.darwino.plugin.userregistry.j2ee.AppUserService;
 
+import com.darwino.commons.Platform;
 import com.darwino.commons.platform.impl.PluginImpl;
 import com.darwino.commons.security.acl.UserService;
 import com.darwino.commons.services.HttpServiceFactory;
@@ -23,9 +25,15 @@ public class UserRegistrationPlugin extends PluginImpl{
 	@Override
 	public void findExtensions(Class<?> serviceClass, List<Object> extensions) {
 		if (HttpServiceFactory.class == serviceClass) {
-			extensions.add(new ProfileServiceFactory());
+			UserRegistrationBean uregBean = Platform.getManagedBeanUnchecked(UserRegistrationBean.BEAN_NAME);
+			if (uregBean != null) {
+				extensions.add(new ProfileServiceFactory());
+			} else {
+				Platform.logManager(getClass().getCanonicalName()).w("No {0} Bean registred!", UserRegistrationBean.BEAN_NAME);
+			}
 		}
 		if (UserService.class == serviceClass) {
+			System.out.println("Loading AppService");
 			extensions.add(new AppUserService());
 		}
 	}
